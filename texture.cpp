fb4fe94a4ac5ebe0f6322fbd6b6a95a55e6cde1e
ibrahimk9000 <ibra1990ski@gmail.com>
2019-11-15 15:57:51 +0100

    fix Missing files

---
+++ texture.cpp
@@ -0,0 +1,41 @@
+
+#include "texture.h"
+std::string texture::cwd(const std::string &res) {
+	char currwd[FILENAME_MAX];
+	std::string resu;
+	
+	if (getcwd(currwd, sizeof(currwd)) != NULL) {
+		resu = currwd;
+		resu += DEFSL + res;
+	}
+	return resu;
+}
+void texture::init(const std::string &path)
+{
+	
+try
+{
+	
+	if (!textu.loadFromFile(cwd(path)))  //load texture if path not found throw error
+		throw 1;
+
+}
+catch (int )
+{
+#ifdef _WIN32
+	std::string err = cwd(path);
+	err += " not found";
+	MessageBoxA(NULL,err.c_str(),"Error!", MB_ICONEXCLAMATION | MB_OK);
+	
+#elif __linux__	
+	std::cerr<<cwd(path)<<" not found";
+	
+#endif
+	throw;
+}
+
+	}
+sf::Texture *texture::map() {
+	
+	return &textu;
+}
---
+++ texture.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <string>
+
+#ifdef _WIN32
+#define getcwd _getcwd
+#define DEFSL "\\res\\"
+#include <direct.h>
+#include <windows.h>
+
+#elif __linux__
+#include <unistd.h>
+#include <iostream>
+#define DEFSL "/res/"
+#endif
+class texture
+{
+private:
+	sf::Texture textu;
+	std::string cwd(const std::string &res);
+
+
+public:
+	void init(const std::string & path);
+
+	sf::Texture *map();
+
+};
